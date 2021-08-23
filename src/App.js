791d53413826baff0e8e218c19aac3596a022d43
Mayank <mayank.joshi@petroitg.com>
2021-08-23 19:43:15 +0530

    @first cimmmit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DataGrid, { Scrolling, Sorting, LoadPanel } from 'devextreme-react/data-grid';
+import { generateData } from './data.js';
+const dataSource = generateData(100000);
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loadPanelEnabled: true
+    };
+    this.onContentReady = this.onContentReady.bind(this);
+  }
+
+  render() {
+    return (
+      <DataGrid
+        elementAttr={{
+          id: 'gridContainer'
+        }}
+        dataSource={dataSource}
+        keyExpr="id"
+        showBorders={true}
+        customizeColumns={this.customizeColumns}
+        onContentReady={this.onContentReady}
+      >
+        <Sorting mode="none" />
+        <Scrolling mode="virtual" />
+        <LoadPanel enabled={this.state.loadPanelEnabled} />
+      </DataGrid>
+    );
+  }
+
+  customizeColumns(columns) {
+    columns[0].width = 70;
+  }
+
+  onContentReady() {
+    this.setState({
+      loadPanelEnabled: false
+    });
+  }
+}
+
+export default App;
